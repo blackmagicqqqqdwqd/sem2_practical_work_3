0624bea30c8a0f0a2e6fd1b30254421713017c82
Danil <danilom31@mail.ru>
2025-05-13 15:27:38 +0300

    edit

--- JavaScript/image.js
+++ JavaScript/image.js
@@ -1,11 +1,11 @@
-// создаем изображение смайлик
+// создаем изображение train
 // рисуем его относительно точки (0, 0)
 function drawSmile(svg) {
-  let smile = svg.append("g")
+  let train = svg.append("g")
       .style("fill", "black")
       .attr("id", "smile");
 
-  smile.append("circle")
+  train.append("circle")
       .attr("cx", 50)
       .attr("cy", 50)
       .attr("r", 47)
@@ -13,40 +13,40 @@ function drawSmile(svg) {
       .attr("stroke", "#000")
       .attr("stroke-width", 1);
 
-  smile.append("rect")
+  train.append("rect")
       .attr("x", 33)
       .attr("y", 40)
       .attr("width", 13)
       .attr("height", 13)
       .attr("fill", "#333");
 
-  smile.append("rect")
+  train.append("rect")
       .attr("x", 47)
       .attr("y", 43)
       .attr("width", 20)
       .attr("height", 10)
       .attr("fill", "#666");
 
-  smile.append("rect")
+  train.append("rect")
       .attr("x", 52)
       .attr("y", 37)
       .attr("width", 3)
       .attr("height", 7)
       .attr("fill", "#444");
 
-  smile.append("circle")  
+  train.append("circle")  
       .attr("cx", 37)
       .attr("cy", 57)
       .attr("r", 3.3)
       .attr("fill", "#000");
 
-  smile.append("circle")  
+  train.append("circle")  
       .attr("cx", 57)
       .attr("cy", 57)
       .attr("r", 3.3)
       .attr("fill", "#000");
 
-  smile.append("ellipse")
+  train.append("ellipse")
       .attr("cx", 53)
       .attr("cy", 32)
       .attr("rx", 3.3)
@@ -54,7 +54,7 @@ function drawSmile(svg) {
       .attr("fill", "gray")
       .attr("opacity", 0.5);
 
-  smile.append("line")
+  train.append("line")
       .attr("x1", 37)
       .attr("y1", 57)
       .attr("x2", 57)
@@ -62,9 +62,9 @@ function drawSmile(svg) {
       .attr("stroke", "#222")
       .attr("stroke-width", 1);
 
-  smile.append("polygon")
+  train.append("polygon")   
       .attr("points", "67,43 67,53 60,53 60,43")
       .attr("fill", "#555");
 
-  return smile;
+  return train;
 }
--- JavaScript/main.js
+++ JavaScript/main.js
@@ -6,7 +6,6 @@ d3.select(window).on("load", function () {
         .attr("width", width)
         .attr("height", height);
 
-
     let option = {
         x_start: 300,
         y_start: 100,
@@ -14,43 +13,25 @@ d3.select(window).on("load", function () {
         y_margin: 10
     }
 
-    //let pict = drawSmile(svg);
-    //pict.attr("transform", `translate(${option.x_start-option.x_margin},${option.y_start-option.y_margin})`); 
-
 });
 
 function clear2() {
     const svg = d3.select("svg");
     svg.selectAll('*').remove();
 }
-function clear3() {
-    d3.select("#smile")
-}
 
 
-
-
-function f() {
-
+function startAnimation() {
 
     const svg = d3.select("svg")
     let pict = drawSmile(svg);
-    //let pict = d3.select("#smile");
-
-    console.log(d3.select("#rotate_finish").property("value"));
+   
     path = drawPath();
     pict.attr("transform", `rotate(${d3.select("#rotate").property("value")},0, 0) scale(${d3.select("#scale").property("value")})`)
         .transition()
         .ease(d3.easeLinear)
         .duration(parseFloat(d3.select("#speedAnimation").property("value")))
         .attrTween('transform', translateAlong(path.node()));
-
-    /*let path = drawPath();	
-    pict.transition()
-    .ease(d3.easeLinear) // установить в зависимости от настроек формы
-    .duration(d3.select("#speedAnimation").property("value"))
-    .attrTween('transform', translateAlong(path.node()));
-    */
 
 }
 
